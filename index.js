1fc692a4ec1465ae3840be052e498e6651a0a4f2
NeroNer0 <nero12750@gmail.com>
2024-10-06 10:53:12 +0700

    index.js

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ const {
 const {
  tz } = moment
 const app = express();
-const port = process.env.PORT || 8040;
+const port = process.env.PORT || 8050;
 const time = tz('Asia/Jakarta').format('HH:mm:ss');
 const currentFilePath = new URL(import.meta.url).pathname;
 
